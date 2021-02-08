542a69351007bb2829dbd425d5a383b0b7ab5567
Yoshikazu Kamimura <yoshikazu.kamimura@gmail.com>
2021-02-08 14:44:00 +0000

    メモアプリ

---
+++ memo.rb
@@ -0,0 +1,27 @@
+require "cvs"
+
+puts "1(新規でメモを作成 2(既存のメモを編集する)"
+
+momo_type = gets.to_s
+
+if memo_type == "1"
+  puts "拡張子を除いたファイルを入力してください"
+  file_name = gets
+  file = File.open("#{file_name}.cvs", "a")
+  
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを押します"
+  memo_message = gets
+  file.put "#{memo_message}"
+  
+  
+  elsif memo_type == "2"
+    puts "拡張子を除いたファイルを入力してください"
+    file_name = gets
+    file = File.open("#{file_name}.cvs", "a")
+    puts "メモしたい内容を記入してください"
+    puts "完了したらCtrl + Dを押します"
+    memo_message = gets
+    file.put "#{memo_message}"
+    
+  end 
