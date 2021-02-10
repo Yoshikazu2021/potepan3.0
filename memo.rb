116b4fc46a37f9f91a5adb2e5e0c08f9fd1f2fa5
Yoshikazu Kamimura <yoshikazu.kamimura@gmail.com>
2021-02-10 01:16:58 +0000

    修正１回目

--- memo.rb
+++ memo.rb
@@ -1,27 +1,26 @@
-require "cvs"
+require "csv"
 
 puts "1(新規でメモを作成 2(既存のメモを編集する)"
 
-momo_type = gets.to_s
+memo_type = gets.to_s
 
 if memo_type == "1"
-  puts "拡張子を除いたファイルを入力してください"
-  file_name = gets
-  file = File.open("#{file_name}.cvs", "a")
-  
-  puts "メモしたい内容を記入してください"
-  puts "完了したらCtrl + Dを押します"
-  memo_message = gets
-  file.put "#{memo_message}"
-  
-  
-  elsif memo_type == "2"
-    puts "拡張子を除いたファイルを入力してください"
+   puts "拡張子を除いたファイルを入力してください"
     file_name = gets
-    file = File.open("#{file_name}.cvs", "a")
+    file = File.open("#{file_name}.csv", "a")
+    
     puts "メモしたい内容を記入してください"
     puts "完了したらCtrl + Dを押します"
     memo_message = gets
     file.put "#{memo_message}"
+
+    elsif memo_type == "2"
+      puts "拡張子を除いたファイルを入力してください"
+      file_name = gets
+      file = File.open("#{file_name}.csv", "a")
+      puts "メモしたい内容を記入してください"
+      puts "完了したらCtrl + Dを押します"
+      memo_message = gets
+      file.put "#{memo_message}"
     
   end 
