b7c1b4e488d3b6eb4c26d938d66b31145184ad46
Yoshikazu Kamimura <yoshikazu.kamimura@gmail.com>
2021-03-08 08:49:07 +0000

    修正２回目

--- memo.rb
+++ memo.rb
@@ -1,26 +1,40 @@
 require "csv"
 
-puts "1(新規でメモを作成 2(既存のメモを編集する)"
+puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
-memo_type = gets.to_s
+memo_number = gets.to_i
 
-if memo_type == "1"
-   puts "拡張子を除いたファイルを入力してください"
-    file_name = gets
-    file = File.open("#{file_name}.csv", "a")
-    
-    puts "メモしたい内容を記入してください"
-    puts "完了したらCtrl + Dを押します"
-    memo_message = gets
-    file.put "#{memo_message}"
+if memo_number == 1
+  puts "拡張子を除いたファイル名を入力してください"
 
-    elsif memo_type == "2"
-      puts "拡張子を除いたファイルを入力してください"
-      file_name = gets
-      file = File.open("#{file_name}.csv", "a")
-      puts "メモしたい内容を記入してください"
-      puts "完了したらCtrl + Dを押します"
-      memo_message = gets
-      file.put "#{memo_message}" 
-    
-  end 
+ file_name = gets.to_s
+
+ puts "メモしたい内容を記入してください"
+ p "完了したら Ctrl+D　を入力します"
+
+memo_type = readlines
+
+CSV.open("#{file_name}.csv", 'w') do |csv|
+  csv << ["#{memo_type}"]
+
+end
+
+
+elsif memo_number == 2
+ puts "既存のcsvファイル名(exist.csv)に追記します"
+
+
+puts "追記したい内容を入力してください"
+p "完了したら Ctrl+D　を入力します"
+
+memo_type = readlines
+
+ CSV.open("exsit.csv", 'a') do |csv|
+
+  csv << ["#{memo_type}"]
+
+ end
+
+else
+  puts "エラー。1か2を選択してください"
+end
